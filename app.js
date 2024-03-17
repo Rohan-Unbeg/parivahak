e4745f1b196cf03fd2b66766acbd5a7fff1f9461
Rohan-Unbeg <rohanunbeg0918@gmail.com>
2024-03-17 20:56:50 +0530

    n2

--- app.js
+++ app.js
@@ -2,15 +2,13 @@ const express = require("express");
 const app = express();
 const homeRoutes = require("./routes/index");
 const path = require("path");
-const PORT = 3000;
+// const PORT = 3000;
 
 app.set("views", "./views");
-// app.use('/stylesheets', express.static('public/stylesheets'));
-// app.use('/images', express.static('public/images'));
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.use("/", homeRoutes);
-app.listen(PORT);
+// app.listen(PORT);
 
 
 module.exports = app;
